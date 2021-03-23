6d8c033b945c43d6861c918dfafc89f127d61e4b
Ovid <curtis_ovid_poe@yahoo.com>
2021-03-23 09:29:42 +0100

    Factor out some common code in request methods.

--- lib/Search/Typesense.pm
+++ lib/Search/Typesense.pm
@@ -199,17 +199,6 @@ sub get_collections {
     return $self->_GET( path => [ 'collections', @collection ] );
 }
 
-sub _GET {
-    my ( $self, %arg_for ) = @_;
-    my $request = $arg_for{request};
-
-    my $url = $self->_url( $arg_for{path} );
-
-    my @args = $request ? ( form => $request ) : ();
-    my $tx   = $self->_ua->get( $url, @args );
-    return $self->_check_for_failure( $tx, $arg_for{return_transaction} );
-}
-
 =head2 C<delete_collection>
 
     my $response = $typesense->delete_collection($collection_name);
@@ -223,13 +212,6 @@ sub delete_collection {
     state $check = compile(NonEmptyStr);
     ($collection) = $check->($collection);
     return $self->_DELETE( path => [ 'collections', $collection ] );
-}
-
-sub _DELETE {
-    my ( $self, %arg_for ) = @_;
-    my $url = $self->_url( $arg_for{path} );
-    my $tx  = $self->_ua->delete($url);
-    return $self->_check_for_failure( $tx, $arg_for{return_transaction} );
 }
 
 =head2 C<create_collection>
@@ -259,20 +241,6 @@ sub create_collection {
         request => $collection_definition
     );
 } ## end sub create_collection
-
-sub _POST {
-    my ( $self, %arg_for ) = @_;
-    my $request = $arg_for{request};
-    my $url     = $self->_url( $arg_for{path} )->query( $arg_for{query} || {} );
-
-    my @args = ref $request
-      ? ( json => $request )    # data structures
-      : $request ? $request     # raw POST content
-      :            croak("POST $url not allowed without a request");
-
-    my $tx = $self->_ua->post( $url, @args );
-    return $self->_check_for_failure( $tx, $arg_for{return_transaction} );
-}
 
 =head2 C<create_document>
 
@@ -331,21 +299,6 @@ sub update_document {
         path    => [ 'collections', $collection, 'documents', $document_id ],
         request => $updates
     );
-}
-
-# Yet, we're getting some serious duplication here. Need to refactorit.
-sub _PATCH {
-    my ( $self, %arg_for ) = @_;
-    my $request = $arg_for{request};
-    my $url     = $self->_url( $arg_for{path} )->query( $arg_for{query} || {} );
-
-    my @args = ref $request
-      ? ( json => $request )    # data structures
-      : $request ? $request     # raw PATCH content
-      :            croak("PATCH $url not allowed without a request");
-
-    my $tx = $self->_ua->patch( $url, @args );
-    return $self->_check_for_failure( $tx, $arg_for{return_transaction} );
 }
 
 =head2 C<delete_document>
@@ -476,11 +429,56 @@ sub delete_all_collections {
     }
 }
 
-sub _check_for_failure {
-    my ( $self, $tx, $return_transaction ) = @_;
-    my $res = $tx->res;
-    unless ( $res->is_success ) {
+sub _GET {
+    my ( $self, %arg_for ) = @_;
+    my $request = $arg_for{request};
+    my @args    = $request ? ( form => $request ) : ();
+    return $self->_handle_request( \%arg_for, \@args );
+}
 
+sub _DELETE {
+    my ( $self, %arg_for ) = @_;
+    return $self->_handle_request( \%arg_for );
+}
+
+sub _POST {
+    my ( $self, %arg_for ) = @_;
+    my $request = $arg_for{request};
+    my @args    = ref $request ? ( json => $request ) : $request;
+    return $self->_handle_request( \%arg_for, \@args );
+}
+
+sub _PATCH {
+    my ( $self, %arg_for ) = @_;
+    my $request = $arg_for{request};
+    my @args    = ref $request ? ( json => $request ) : $request;
+    return $self->_handle_request( \%arg_for, \@args );
+}
+
+sub _handle_request {
+    my ( $self, $arg_for, $args ) = @_;
+
+    # We must only be called by methods like _GET, _POST, _DELETE, and so on.
+    # We strip the package name and leading underscore
+    # (Search::Typesense::_GET becomes GET) and then we call lc() on what's
+    # left. That becomes our HTTP verb and the $check verifies that this is an
+    # allowed verb.
+    my ( undef, undef, undef, $method ) = caller(1);
+    $method =~ s/^.*::_//;
+    state $check = compile( Enum [qw/get delete post patch/] );
+    ($method) = $check->( lc $method );
+
+    # make the actual request, passing a query string, if any, and passing any
+    # args, if any (those can become part of a query string for GET, or part
+    # of the body for other HTTP verbs
+    my @args = $args ? @$args : ();
+    my $url = $self->_url( $arg_for->{path} )->query( $arg_for->{query} || {} );
+    my $tx  = $self->_ua->$method( $url, @args );
+    my $res = $tx->res;
+
+    # If the response is not succesful, return nothing if it's a 404.
+    # Otherwise, croak()
+    unless ( $res->is_success ) {
         return if ( $res->code // 0 ) == 404;
         my $message = $res->message // '';
 
@@ -489,7 +487,8 @@ sub _check_for_failure {
         my $url    = $tx->req->url;
         croak("'$method $url' failed: $message. $body");
     }
-    return $return_transaction ? $tx : $tx->res->json;
+
+    return $arg_for->{return_transaction} ? $tx : $tx->res->json;
 } ## end sub _check_for_failure
 
 =head1 AUTHOR
